8d0ea73ae9cfa439d0e9008fc8ffba372665615f
Lea <lmtejedor@gmail.com>
2022-05-07 19:03:18 -0400

    reorder pages

--- flaskr/art.py
+++ flaskr/art.py
@@ -16,3 +16,48 @@ def index():
 def connect():
     db = get_db()
     return render_template('art/connect.html')
+
+@bp.route('/give')
+def give():
+    db = get_db()
+    return render_template('art/give.html')
+
+
+@bp.route('/address')
+def address():
+    db = get_db()
+    return render_template('art/address.html')
+
+@bp.route('/check')
+def check():
+    db = get_db()
+    return render_template('art/check.html')
+
+@bp.route('/pay')
+def pay():
+    db = get_db()
+    return render_template('art/pay.html')
+
+
+@bp.route('/checknum')
+def checknum():
+    db = get_db()
+    return render_template('art/checknum.html')
+
+
+@bp.route('/wordselection')
+def wordselection():
+    db = get_db()
+    return render_template('art/wordselection.html')
+
+
+@bp.route('/loading')
+def loading():
+    db = get_db()
+    return render_template('art/loading.html')
+
+
+@bp.route('/claim')
+def claim():
+    db = get_db()
+    return render_template('art/claim.html')
