c0808261ca1d45168f31ed023bc47acecd475643
Akshit Singla <akshitsingla@dhcp-10-29-56-184.dyn.MIT.EDU>
2022-05-07 23:03:10 -0400

    Adding metamask connection

--- flaskr/static/js/script.js
+++ flaskr/static/js/script.js
@@ -1,11 +1,27 @@
-// (GLOBAL) STATE VARIABLES
-var WALLET_ADDRESS = "";
-var SEEDS = [];
-var SIA_IDs = [];
 // UTILITY FUNCTIONS
 const navigateTo = function(pageName) {
     window.location = `http://localhost:5000/${pageName}`;
 }
+const getQueryStrings = function () { 
+    var assoc  = {};
+    var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
+    var queryString = location.search.substring(1); 
+    var keyValues = queryString.split('&'); 
+  
+    for(var i in keyValues) { 
+      var key = keyValues[i].split('=');
+      if (key.length > 1) {
+        assoc[decode(key[0])] = decode(key[1]);
+      }
+    } 
+  
+    return assoc; 
+  } 
+// (GLOBAL) STATE VARIABLES
+var WALLET_ADDRESS = getQueryStrings()["WALLET_ADDRESS"];
+var SEEDS = [];
+var SIA_IDs = [];
+var IMAGE_ADDRESS = "1.jpeg"
 // UI BUTTONS
 const visitAsteroidButton = function() {
     console.log(`visitAsteroidButton clicked`);
@@ -20,16 +36,24 @@ const connectWalletButton = function() {
 const connectMetaMaskButton = function(walletAddress) {
     console.log(`connectMetaMaskButton clicked`);
     // CONNECT WITH METAMASK
-    // TODO
-    // NAVIGATE
-    navigateTo("wordselection");
+    const enableMetaMask = function() {
+        return new Promise(function(resolve, reject) {
+            resolve(window.ethereum.enable());
+        });
+    }
+    enableMetaMask()
+        .then(function(response) {
+            WALLET_ADDRESS = response[0];
+            // NAVIGATE
+            navigateTo(`wordselection?WALLET_ADDRESS=${WALLET_ADDRESS}`);
+        });
 }
 const takeArtSeedButton = function(seeds) {
     console.log(`takeArtSeedButton clicked`);
     // STORE GLOBAL VARIABLE
     // TODO
     // NAVIGATE
-    navigateTo("pay");
+    navigateTo(`pay?WALLET_ADDRESS=${WALLET_ADDRESS}`);
 }
 const generateArtButton = function(imageFilename) {
     console.log(`generateArtButton clicked`);
@@ -38,20 +62,20 @@ const generateArtButton = function(imageFilename) {
     // DISPLAY IMAGE
     // TODO
     // NAVIGATE
-    navigateTo("claim");
+    navigateTo(`claim?WALLET_ADDRESS=${WALLET_ADDRESS}`);
 }
 const claimArtButton = function() {
     console.log(`claimArtButton clicked`);
     // UPLOAD IMAGE TO SIA
-    uploadToSia("1.jpeg")
+    uploadToSia(IMAGE_ADDRESS)
         .then(function(siaId) {
             console.log(`siaId: ${siaId}`);
             // GENERATE NFT WITH SIA_ID
-            return makeNFT("0xE7904160ca9500D2382B9163aEc20DDb0F9dCCC3", siaId);
+            return makeNFT(WALLET_ADDRESS, siaId);
         })
         .then(function(txId) {
             console.log(`txId: ${txId}`);
         });
     // NAVIGATE
-    // navigateTo("confirmation");
+    // TODO: navigateTo("confirmation");
 }
